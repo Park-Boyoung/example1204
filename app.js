863786c8395b471d45eef50060447c8bdcf20d07
Park-Boyoung <bypark9411@gmail.com>
2022-12-04 15:42:07 +0900

    example

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
+// @ts-check
+
 const express = require("express");
+
+const mainRouter = require("./routes/main");
+const userRouter = require("./routes/users");
 
 const app = express();
 
+const PORT = 7000;
+
+// ejs setting
+app.set("view engine", "ejs");
+
+// router setting
+app.use("/", mainRouter);
+app.use("/users", userRouter);
+
 app.listen(PORT, () => {
-  console.log(`서버는 ${PORT}번에서 실행 중입니다!`);
+  console.log(`${PORT} 포트에서 서버 실행 중`);
 });
